985a4ac605a330f09a1f1022de7153d8ad816ebf
rascalQ7 <riokas1@gmail.com>
2020-08-30 21:48:12 +0300

    Add post form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.forms import modelform_factory
+
+from tiny_urls.models import TinyURL
+
+
+TinyURLForm = modelform_factory(TinyURL, exclude=[TinyURL.name])
 
 
 def home(request):
-    return render(request, "website/home.html")
+    if request.method == "POST":
+        form = TinyURLForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = TinyURLForm()
+    return render(request, "website/home.html", {"form": form})
