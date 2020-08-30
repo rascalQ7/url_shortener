0c4b8304bf6cfff86db5108238b911414518acb8
rascalQ7 <riokas1@gmail.com>
2020-08-30 20:53:49 +0300

    Redirect /r/tiny url to original url

--- tiny_urls/models.py
+++ tiny_urls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TinyURL(models.Model):
     name = models.CharField(max_length=100)
-    originalURL = models.CharField(max_length=1000)
+    original_url = models.CharField(max_length=1000)
 
     def __str__(self):
-        return  self.name
+        return self.name
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from website.views import home
+from tiny_urls.views import external_redirection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('r/<str:tiny_url>', external_redirection),
 ]
