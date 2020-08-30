1439bc2a90ddc66088725ece55807286b6d7a801
rascalQ7 <riokas1@gmail.com>
2020-08-30 21:02:36 +0300

    Return 404 if tiny url does not exist

--- tiny_urls/views.py
+++ tiny_urls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from .models import TinyURL
 
 
 def external_redirection(request, tiny_url):
-    url = TinyURL.objects.get(name=tiny_url)
+    url = get_object_or_404(TinyURL, name=tiny_url)
     return redirect('http://' + url.original_url)
