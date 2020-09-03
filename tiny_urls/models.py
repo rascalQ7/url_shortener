bba78cf5e7f181764213f5b1268a82a215b61ff5
rascalQ7 <60157957+rascalQ7@users.noreply.github.com>
2020-09-03 22:11:52 +0300

    Expand tinyUrl model with configurable id as primary key (#6)
    Create random id on post form

--- tiny_urls/models.py
+++ tiny_urls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class TinyURL(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     original_url = models.CharField(max_length=1000)
 
