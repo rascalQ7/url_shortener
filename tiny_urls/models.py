82312d5bc7c683ee414212df8f4cc70f9a90c57f
rascalQ7 <60157957+rascalQ7@users.noreply.github.com>
2020-09-04 16:51:25 +0300

    Imrpove redirections (#7)
    * Add http to dns on redirection
    * Improve code
    * Update tiny urls path

--- tiny_urls/models.py
+++ tiny_urls/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class TinyURL(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=10)
     original_url = models.CharField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
