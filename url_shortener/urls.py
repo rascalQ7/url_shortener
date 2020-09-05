d3277e33f60406b9e62f41904ffb9c4c5c995310
rascalQ7 <60157957+rascalQ7@users.noreply.github.com>
2020-09-05 22:09:30 +0300

    Add base html (#11)

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import home, login_user, sign_up_user, logout_user
+from website.views import home, dashboard, login_user, sign_up_user, logout_user
 from tiny_urls.views import external_redirection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('login/', login_user, name='login'),
     path('signup/', sign_up_user, name='sign_up'),
     path('logout/', logout_user, name='logout'),
