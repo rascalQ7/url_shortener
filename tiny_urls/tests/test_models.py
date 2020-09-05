a2446070e9b95f533ece3409ac18a44989131d57
rascalQ7 <riokas1@gmail.com>
2020-09-05 20:39:06 +0300

    Add auth views and templates

--- tiny_urls/tests/test_models.py
+++ tiny_urls/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from website.views import convert_number_to_base_string
+from tiny_urls.models import TinyURL
 from url_shortener import constant
 
 
@@ -9,13 +9,13 @@ class TestViews(TestCase):
         is_all_strings_unique = True
         base_strings_list = []
         for x in range(len(constant.BASE)):
-            base_string = convert_number_to_base_string(constant.BASE_RANGE_LOWER + x)
+            base_string = TinyURL.convert_number_to_base_string(constant.BASE_RANGE_LOWER + x)
             if base_string in base_strings_list:
                 is_all_strings_unique = False
             base_strings_list.append(base_string)
         assert is_all_strings_unique
 
     def test_base_converter_generates_same_size_url(self):
-        lower = convert_number_to_base_string(constant.BASE_RANGE_LOWER)
-        upper = convert_number_to_base_string(constant.BASE_RANGE_UPPER)
+        lower = TinyURL.convert_number_to_base_string(constant.BASE_RANGE_LOWER)
+        upper = TinyURL.convert_number_to_base_string(constant.BASE_RANGE_UPPER)
         assert len(lower) == len(upper)
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import home
+from website.views import home, login_user, sign_up_user, logout_user
 from tiny_urls.views import external_redirection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('login/', login_user, name='login'),
+    path('signup/', sign_up_user, name='sign_up'),
+    path('logout/', logout_user, name='logout'),
     path('<str:tiny_url>', external_redirection),
 ]
